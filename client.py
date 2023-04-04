dd03839d16ef811a4fea2258aa63bf8a69066e3a
Vishmaya <72074398+VishmayaGithub@users.noreply.github.com>
2023-04-04 17:53:02 +0530

    Update client.py

--- client.py
+++ client.py
@@ -5,7 +5,7 @@ from tkinter import ttk
 import ftplib
 import os
 import ntpath #This is used to extract filename from path
-
+from ftplib import FTP
 from tkinter import filedialog
 from pathlib import Path
 
